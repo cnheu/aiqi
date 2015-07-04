9e1af7e4186448c975a27023b00fd52b029e35cb
cnheu <tofa.nheu@gmail.com>
2015-07-04 21:37:59 +1000

    change gitignore to remove myvenv

--- blog/views.py
+++ blog/views.py
@@ -45,3 +45,6 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def index(request):
+    return render(request, 'blog/index.html', {'index':index})
