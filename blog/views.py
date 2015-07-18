7d8b797585f0a3897100e407f683d11396f95225
cnheu <tofa.nheu@gmail.com>
2015-07-19 01:28:03 +1000

    Add blog delete and publish functions

--- blog/views.py
+++ blog/views.py
@@ -24,7 +24,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
@@ -39,7 +39,7 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
@@ -51,3 +51,17 @@ def index(request):
 
 def registration(request):
     return render(request, 'blog/registration.html', {'registration': registration})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts':posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete() # delete a django model (or entry in the table)
+    return redirect('blog.views.post_list')
